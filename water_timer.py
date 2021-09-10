bce3c756a20551e42fac03fe7f686657c5ed06f5
RastogiUtkarsh <60594424+RastogiUtkarsh@users.noreply.github.com>
2021-09-11 03:02:32 +0530

    Update water_timer.py

--- water_timer.py
+++ water_timer.py
@@ -4,8 +4,9 @@ from plyer import notification
 if __name__ == "__main__":
     while True:
         notification.notify(
-            title="Please Drink Water Boss",
-            message="Paani peele aalsi aadmi",
+            title="Please Drink Water",
+            message="Water is more important than this!!",
+            app_icon = "location of paani(water).ico"
             timeout=10
         )
         time.sleep(60*60)
